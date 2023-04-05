93ff424c3cad7fd220df1463f4b535a7a99a864d
Chris Myers <70340885+schrismyers@users.noreply.github.com>
2023-04-05 12:39:34 -0600

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,3 +1,7 @@
+"""
+python file is for flask server
+"""
+
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
@@ -6,19 +10,22 @@ app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
+    """ This function uses the translator package on the webpage """
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    frenchText = translator.english_to_french(textToTranslate)
+    return f"Translated text to French: {frenchText}"
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
+    """ This function uses the translator package on the webpage """
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    englishText = translator.french_to_english(textToTranslate)
+    return f"Translated text to English: {englishText}"
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    """ This function renders the webpage template using index.html """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
